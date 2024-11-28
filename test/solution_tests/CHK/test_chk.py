3e59c7930155223cb6e7130bde02aac89f935b70
sbx_user1051 <sbx_user1051@169.254.89.206>
2024-11-28 17:14:28 +0000

    Thu Nov 28 17:14:28 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,6 +1,9 @@
-from lib.solutions.CHK import hello_solution
+from lib.solutions.CHK.checkout_solution import checkout
 
 
 class TestChk():
     def test_chk(self):
-        pass
+        assert checkout("ABCD") == 115
+    def test_chk_offers(self):
+        assert checkout("AAA") == 130
+        assert checkout("BB") == 45
