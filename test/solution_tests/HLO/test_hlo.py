8c5fd452c405d86e7293b2711f7ad3868320a95d
sbx_user1051 <sbx_user1051@169.254.89.206>
2024-11-28 17:03:41 +0000

    Thu Nov 28 17:03:41 UTC 2024

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -2,5 +2,8 @@ from lib.solutions.HLO import hello_solution
 
 
 class TestHlo():
-    def test_hlo(self):
+    def test_hlo_r1(self):
         assert isinstance(hello_solution.hello("abc"),str)
+
+    def test_hlo_r2(self):
+        assert hello_solution.hello("John") == "Hello, John!"
