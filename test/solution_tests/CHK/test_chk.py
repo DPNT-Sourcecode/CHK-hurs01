ea49a8bcbcd1d3427aad28195672e7a9d398a133
sbx_user1051 <sbx_user1051@169.254.58.130>
2024-11-28 19:37:24 +0000

    Thu Nov 28 19:37:24 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,10 +4,13 @@ from lib.solutions.CHK.checkout_solution import checkout
 class TestChk():
     def test_chk(self):
         assert checkout("ABCD") == 115
-    def test_chk_offers_A(self):
+    def test_chk_offers(self):
         assert checkout("AAA") == 130
         assert checkout("AAAAA") == 200
         assert checkout("AAAAAAA") == 300
         assert checkout("BB") == 45
+        assert checkout("EEB") == 80
+    def test_chk_offers_multi(self):
+        assert checkout("EEBB") == 110
     def test_chk_invalid(self):
         assert checkout("ZYX") == -1
