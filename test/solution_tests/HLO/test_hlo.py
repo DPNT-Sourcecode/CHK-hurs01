a763040d5a2a520d95166ce880caafa19529cf23
sbx_user1051 <sbx_user1051@169.254.89.206>
2024-11-28 17:00:15 +0000

    Thu Nov 28 17:00:15 UTC 2024

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,8 +1,6 @@
-from lib.solutions.SUM import sum_solution
+from lib.solutions.HLO import hello_solution
 
 
-class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
-        assert sum_solution.compute(100,100) == 200
-        assert sum_solution.compute(0,0) == 0
+class TestHlo():
+    def test_hlo(self):
+        assert isinstance(hello_solution.hello("abc"),str)
